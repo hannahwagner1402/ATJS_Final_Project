351ae6dbcd54b2a5395e89cfb60a42a511fed442
hannahwagner1402 <92428401+hannahwagner1402@users.noreply.github.com>
2023-02-19 13:35:34 +0100

    Added new Icons and finished styles

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ const result = ref(null)
 
 
 
+// defining the logic of the different outcomes of the game
 const outcomes = {
   fire:{
     fire: 'draw',
@@ -30,6 +31,7 @@ const outcomes = {
   }
 }
 
+// calculating the overall winpercentage based on the scores
 const winPercentage = computed(() => {
   const total = winScore.value + drawScore.value + loseScore.value
 
@@ -37,18 +39,21 @@ const winPercentage = computed(() => {
   return total ? (winScore.value/ total) * 100 : 0
 })
 
+//
 const play = c => {
   choice.value = c
 
+  // defining choices/ randomized computer choice
   const choices = ['fire','water','earth']
   const random = Math.floor(Math.random()*3)
   compChoice.value = choices[random]
 
   const outcome = outcomes[c][compChoice.value]
 
+  // returning the outcome of the game
   if (outcome === 'win') {
     winScore.value++
-    result.value = 'You won :D'
+    result.value = 'You won! :)'
   } else if (outcome === 'loss') {
     loseScore.value++
     result.value = 'You lost :/'
@@ -59,6 +64,7 @@ const play = c => {
   SaveScores()
 }
 
+// locally saving win/lose/draw score
 const SaveScores = () => {
   localStorage.setItem('winScore', winScore.value)
   localStorage.setItem('drawScore', drawScore.value)
@@ -71,51 +77,49 @@ const LoadGame = () => {
   loseScore.value = parseInt(localStorage.getItem('loseScore')) || 0
 }
 
+//Reseting Game
 const ResetRound = () => {
   choice.value = null
   compChoice.value = null
   result.value = null
 }
 
+//Loading the game
 onMounted(() => {
   LoadGame()
-  window.addEventListener('keypress', e => {
-    if (e.key === 'r'){
-      ResetRound()
-    }
-  })
 })
 
 </script>
-
+//using tailwind to add css
 <template>
-<div class="bg-green-900 text-white text-center min-h-screen flex flex-col">
-  <header class = "container mx-auto p-6 bg-green-600">
-    <h1 class="text-5xl font-semibold mb-4 mt-3">Elemental Rock Paper Scissors</h1>
-    <h1 class="text-2xl"><div class = "flex-row justify-center"> Choose between <text class="text-amber-400 font-bold drop-shadow-lg m-0 p-0">FIRE</text>,
-      <text class="text-blue-100 font-bold drop-shadow-lg">WATER</text> and <text class="text-lime-200 font-bold drop-shadow-lg">EARTH</text></div></h1>
+<div class="bg-bggreen text-white text-center min-h-screen flex flex-col ">
+  <header class = "container mx-auto p-6 bg-green-900 drop-shadow-lg">
+    <h1 class="text-5xl font-semibold mb-4 mt-3">ELEMENTAL ROCK PAPER SCISSORS</h1>
+    <h1 class="text-2xl font-light"><div class = "flex-row justify-center"> Choose between <text class="text-orange drop-shadow-lg m-0 p-0">FIRE</text>,
+      <text class="text-blue  drop-shadow-lg">WATER</text> and <text class="text-green drop-shadow-lg">EARTH</text></div></h1>
   </header>
 
-  <main class = "container mx-auto p-0 flex-1 bg-green-600">
-    <div v-if="choice === null" class="flex items-center justify-center p-6 -mx-6">
+
+  <main class = "container mx-auto p-0 flex-1 bg-lightgreen">
+    <div v-if="choice === null" class="flex items-center justify-center p-6 -mx-6 mt-4">
 
       <button
           @click="play('fire')"
-      class="bg-white rounded-full drop-shadow-2xl w-64 p-12
+      class="bg-white rounded-full drop-shadow-2xl w-60 p-11
       mx-6 transition-colors duration-300 hover:bg-amber-100">
         <img src="../assets/fire.png" alt="Fire" class="w-full">
       </button>
 
       <button
           @click="play('water')"
-          class="bg-white rounded-full drop-shadow-2xl w-64 p-12
+          class="bg-white rounded-full drop-shadow-2xl w-60 p-11
       mx-6 transition-colors duration-300 hover:bg-blue-100 shadow-inner w-64 p-12">
         <img src="../assets/water.png" alt="Water" class="w-full">
       </button>
 
       <button
           @click="play('earth')"
-          class="bg-white rounded-full drop-shadow-2xl w-64 p-12
+          class="bg-white rounded-full drop-shadow-2xl w-60 p-11
       mx-6 transition-colors duration-300 hover:bg-lime-200">
         <img src="../assets/earth.png" alt="Earth" class="w-full">
       </button>
@@ -123,28 +127,28 @@ onMounted(() => {
     </div>
 
     <div v-else>
-      <div class = "bg-green-700 pt-20 pb-20 ml-20 mr-20 ">
-      <div class="text-3xl mb-4">
-        You chose <span class="text-purple-200 font-sans md:font-serif italic">{{choice}}</span>
+      <div class = "bg-lightgreen mx-auto ">
+      <div class="text-3xl mb-4 mt-10 font-light">
+        You chose <span class="font-medium italic">{{choice}}</span>
       </div>
-      <div class="text-3xl mb-4">
-        The computer chose <span class="text-yellow-200 font-sans md:font-serif italic">{{compChoice}}</span>
+      <div class="text-3xl mb-4 font-light">
+        And I chose <span class="font-medium italic">{{compChoice}}</span>
       </div>
 
-      <div class="text-6xl mb-12">
+      <div class="text-6xl mb-7 font-medium">
         {{result}}
       </div>
 
-      <button @click="ResetRound" class="bg-green-500 drop-shadow-lg text-xl font-semibold p-5 pl-10 pr-10">RESET GAME</button>
+      <button @click="ResetRound" class="bg-white drop-shadow-lg text-xl font-light text-black p-6 pl-10 pr-10 mb-9">RESET GAME</button>
     </div>
     </div>
 
-    <div class="mt-7 text-3xl mb-4">
-      <text class = "text-lime-200 drop-shadow-lg">wins:</text> {{winScore}}, <text class="text-blue-300 drop-shadow-lg">draws:</text> {{drawScore}},
-      <text class = "text-amber-400 drop-shadow-lg" >losses:</text> {{loseScore}}
+    <div class="text-3xl mb-4">
+      <text class = "text-bggreen drop-shadow-lg">wins:</text> {{winScore}}, <text class="text-darkblue drop-shadow-lg">draws:</text> {{drawScore}},
+      <text class = "text-orange drop-shadow-lg" >losses:</text> {{loseScore}}
     </div>
 
-    <div class="text-lg mb-10">
+    <div class="text-lg font-light mb-5">
       Win rate: {{Math.round(winPercentage)}}%
 
     </div>
@@ -152,14 +156,14 @@ onMounted(() => {
 
   </main>
 
-  <div class = "bg-green-900 p-6 rounded-b-lg drop-shadow-xl">
+  <div class = "bg-bggreen rounded-b-lg drop-shadow-xl">
 
-    <h1 class="text-3xl font-bold mt-0 text-lime-50 drop-shadow-md">RULES:</h1>
+    <h1 class="text-3xl font-bold mt-3 drop-shadow-md">RULES:</h1>
 
-    <div class="text-lg mt-3 mb-3">
+    <div class="text-lg mt-2 mb-3">
 
-      <p class=" text-gray-200 font-sans md:font-serif">Fire beats earth, earth beats water and water beats fire. </p>
-      <p class ="text-gray-200 font-semibold font-sans md:font-serif">Have fun playing!</p>
+      <p class=" font-light">Fire beats earth, earth beats water and water beats fire. </p>
+      <p class ="font-light">Have fun playing!</p>
 
     </div>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,17 @@ module.exports = {
 
   ],
   theme: {
+      colors: {
+          bggreen: '#065f46',
+          lightgreen: '#6D9D87',
+          white:'#FFFFFF',
+          orange: '#FFB613',
+          blue: '#699AE2',
+          green: '#87CD99',
+          darkblue: '#2A4C8A',
+          black: '#000000'
+
+      },
     extend: {},
   },
   plugins: [],
